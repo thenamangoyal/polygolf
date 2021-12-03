168aabaa6664de356d60f665025a449ccd515fb3
Mia Chiquier <miachiquier@Mias-MacBook-Pro.local>
2021-12-03 18:06:01 -0500

    Add array for checking if in water

--- players/g4_player.py
+++ players/g4_player.py
@@ -1,8 +1,11 @@
+import time
 import numpy as np
 import sympy
 import logging
 from typing import Tuple
 import constants
+from shapely import geometry
+import pdb
 
 
 class Player:
@@ -21,6 +24,95 @@ class Player:
         self.risk = 0.1
         self.simulate_times = 10
         self.tolerant_times = self.simulate_times * self.risk
+        self.remember_middle_points = []
+
+        self.turn = 0
+
+    def water_boolean(self, poly, grid_points):
+        water_grid = []
+        
+        for i,row in enumerate(grid_points): 
+            water_grid.append([])
+            for j,point in enumerate(row):
+                thebool = poly.contains(point)
+                
+                water_grid[i].append(thebool)
+        print(np.array(water_grid))
+
+
+
+    def make_grid(self, golf_map: sympy.Polygon,target: sympy.geometry.Point2D,
+        curr_loc: sympy.geometry.Point2D, prev_loc: sympy.geometry.Point2D):
+
+        poly=geometry.Polygon([p.x, p.y] for p in golf_map.vertices)
+        print("convert")
+
+        (xmin, ymin, xmax, ymax) = golf_map.bounds
+        list_of_lists = []
+        list_of_distances=[]
+        
+        queue = []
+        dimension = 10
+        allowed_distance = 5
+        threshold = 20.0
+        amt=1
+        grid_of_scores = np.array(np.ones((dimension,dimension))*100000)
+        
+        
+        xmin = float(xmin)
+        ymin = float(ymin)
+        xmax = float(xmax)
+        ymax = float(ymax)
+        
+        print(xmin,xmax,ymin,ymax)
+        
+        xcoords,ycoords = np.meshgrid(np.linspace(xmin,xmax, dimension),np.linspace(ymin,ymax, dimension))
+
+        for x_index in range(len(xcoords)):
+            list_of_lists.append([])
+            list_of_distances.append([])
+            for y_index in range(len(ycoords)):
+                #considered_point = sympy.geometry.Point2D(xcoords[y_index,x_index],ycoords[y_index,x_index])
+                considered_point = geometry.Point(xcoords[y_index,x_index],ycoords[y_index,x_index])
+                list_of_lists[x_index].append(considered_point)
+                #thedistance = curr_loc.distance(considered_point)
+                #list_of_distances[x_index].append(thedistance)
+                #print(float(considered_point[0]),float(considered_point[1]), float(curr_loc[0]), float(curr_loc[1]))
+        print("test1")
+        self.water_boolean(poly, list_of_lists)
+        """BFS stuff"""
+        if thedistance < threshold:
+            grid_of_scores[x_index,y_index] = amt
+        return "hi"
+
+    """graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5')    # function calling"""
 
     def play(self, score: int, golf_map: sympy.Polygon, target: sympy.geometry.Point2D,
              curr_loc: sympy.geometry.Point2D, prev_loc: sympy.geometry.Point2D,
@@ -39,6 +131,11 @@ class Player:
         Returns:
             Tuple[float, float]: Return a tuple of distance and angle in radians to play the shot
         """
+        print("test")
+        if self.turn == 0:
+            a = self.make_grid(golf_map,target,curr_loc, prev_loc)
+
+        self.turn += 1
         # 1. always try greedy first
         required_dist = curr_loc.distance(target)
         roll_factor = 1. + constants.extra_roll
@@ -59,36 +156,45 @@ class Player:
                     break
 
         if is_greedy:
+            self.logger.info(str(self.turn) + "select greedy strategy to go")
             return (distance, angle)
 
         # 2. if we cannot use greedy, we try to find the points intersected with the golf map
-        circle = sympy.Circle(curr_loc, distance)
-        # TODO: cost about 6-8 seconds, too slow
-        intersect_points_origin = circle.intersection(golf_map)
+        if prev_admissible is None or prev_admissible or not self.remember_middle_points:
+            circle = sympy.Circle(curr_loc, distance)
+            # TODO: cost about 6-8 seconds, too slow
+            intersect_points_origin = circle.intersection(golf_map)
 
-        # delete useless intersection points
-        intersect_points = []
-        for i in range(len(intersect_points_origin)):
-            intersect_distance = intersect_points_origin[i].distance(target)
-            if intersect_distance <= required_dist:
-                intersect_points.append(intersect_points_origin[i])
+            intersect_points_num = len(intersect_points_origin)
+            temp_middle_points = []
 
-        intersect_points_num = len(intersect_points)
-        middle_points = []
+            for i in range(intersect_points_num):
+                for j in range(i + 1, intersect_points_num):
+                    middle_point = sympy.Point2D(float(intersect_points_origin[i].x + intersect_points_origin[j].x) / 2,
+                                                 float(intersect_points_origin[i].y + intersect_points_origin[j].y) / 2)
+                    # find points that in the golf map polygon
+                    if golf_map.encloses(middle_point):
+                        temp_middle_points.append(middle_point)
 
-        for i in range(intersect_points_num):
-            for j in range(i + 1, intersect_points_num):
-                middle_point = sympy.Point2D((intersect_points[i].x + intersect_points[j].x) / 2,
-                                                   (intersect_points[i].y + intersect_points[j].y) / 2)
-                # find points that in the golf map polygon
-                # TODO: cost about 8 seconds, too slow
-                if golf_map.encloses_point(middle_point):
-                    # print(i, j)
-                    middle_points.append(middle_point)
+            if len(temp_middle_points) == 0:
+                self.logger.error(str(self.turn) + "cannot find any middle point, BUG!!!")
+                return (distance, angle)
 
-        if len(middle_points) == 0:
-            self.logger.error("cannot find any middle point, BUG!!!")
-            return (distance, angle)
+            # if there are many ways to go, delete the points that can go back
+            middle_points = []
+            for i, middle_point in enumerate(temp_middle_points):
+                if middle_point.distance(target) > required_dist:
+                    continue
+                middle_points.append(middle_point)
+
+            # if there we delete every point in temp_middle_points,
+            # which means we could go longer ways than expected, we need to add back those points
+            if len(middle_points) == 0:
+                middle_points = temp_middle_points
+
+            self.remember_middle_points = middle_points
+        else:
+            middle_points = list(self.remember_middle_points)
 
         middle_points_num = len(middle_points)
         mid_to_target_distance = [0] * middle_points_num
@@ -97,8 +203,7 @@ class Player:
 
         distance_sorted_indexes = sorted(range(middle_points_num), key=lambda x: mid_to_target_distance[x])
 
-        # target_info: [failed_times, distance from next_shot to target]
-        target_info = [[0, 0]] * middle_points_num
+        middle_failed_times = [0] * middle_points_num
 
         midd_index = -1
         for i in distance_sorted_indexes:
@@ -107,34 +212,31 @@ class Player:
             for _ in range(self.simulate_times):
                 is_succ, final_point = self.simulate_once(distance, angle, curr_loc, golf_map)
                 if not is_succ:
-                    target_info[i][0] += 1
-                    if target_info[i][0] > self.tolerant_times:
-                        target_info[i][0] = -1
+                    middle_failed_times[i] += 1
+                    if middle_failed_times[i] > self.tolerant_times:
+                        middle_failed_times[i] = -1
                         break
-                target_info[i][1] += final_point.distance(target)
 
-            if target_info[i][0] != -1:
+            if middle_failed_times[i] != -1:
                 midd_index = i
                 break
 
         desire_distance = distance
         if midd_index != -1:
+            self.logger.info(str(self.turn) + "select largest distance to middle point to go")
             desire_angle = sympy.atan2(middle_points[midd_index].y - curr_loc.y,
                                        middle_points[midd_index].x - curr_loc.x)
+
             return (desire_distance, desire_angle)
 
-        # 3. if everywhere is not safe, we try middle_points and choose the middle point that closest to target
-        mid_to_target_distance = [0] * middle_points_num
-        for i, middle_point in enumerate(middle_points):
-            mid_to_target_distance[i] = middle_point.distance(target)
-
-        sorted_indexes = sorted(range(middle_points_num), key=lambda x: mid_to_target_distance[x])
-        closest_index = sorted_indexes[0]
+        # 3. if middle points are still not safe, choose the closest one to the target
+        closest_index = distance_sorted_indexes[0]
         closest_middle_point = middle_points[closest_index]
 
         curr_to_mid = closest_middle_point.distance(curr_loc)
         desire_distance = sympy.Min(constants.max_dist + self.skill, curr_to_mid / roll_factor)
         desire_angle = sympy.atan2(closest_middle_point.y - curr_loc.y, closest_middle_point.x - curr_loc.x)
+        self.logger.info(str(self.turn) + "risky!!! select closest middle point to go")
         return (desire_distance, desire_angle)
 
     def simulate_once(self, distance, angle, curr_loc, golf_map):
@@ -157,4 +259,3 @@ class Player:
 
         segment_land = sympy.geometry.Segment2D(landing_point, final_point)
         return golf_map.encloses(segment_land), final_point
-
