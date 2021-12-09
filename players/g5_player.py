e67dbf2141c8bc50ac646f5c9bf70ad1b31dd9ec
Sam Raab <raabsm@gmail.com>
2021-12-09 17:39:54 -0500

    before new heuristic

--- players/g5_player.py
+++ players/g5_player.py
@@ -17,13 +17,18 @@ def line_in_polygon(point_a, point_b, polygon):
     return if_encloses
 
 
-def is_roll_in_polygon(point_a, distance, angle, polygon):
-    curr_point = Point(point_a.x + distance * np.cos(angle),
-                       point_a.y + distance * np.sin(angle))
+def get_roll_final_point(point_a, distance, angle):
     final_point = Point(
         point_a.x + (1.1) * distance * np.cos(angle),
         point_a.y + (1.1) * distance * np.sin(angle))
+    return final_point
 
+
+def is_roll_in_polygon(point_a, distance, angle, polygon):
+    curr_point = Point(point_a.x + distance * np.cos(angle),
+                       point_a.y + distance * np.sin(angle))
+
+    final_point = get_roll_final_point(point_a, distance, angle)
     start = time()
     if_encloses = line_in_polygon(curr_point, final_point, polygon)
     end = time()
@@ -41,13 +46,55 @@ def convert_sympy_shapely(point):
     return Point(point.x, point.y)
 
 
+def predict_num_shots(distance_to_hole, skill):
+    max_distance = 200 + skill
+    distance_groups = [0, 20, 100] + ([200 + skill - 20] * 10)
+    for i in range(len(distance_groups)):
+        if int(distance_to_hole) <= distance_groups[i]:
+            dg = i
+            break
+
+    if dg != 0:
+        next_min = distance_groups[dg-1]
+        decimal = (distance_to_hole - next_min) / (distance_groups[dg] - next_min)
+        dg += decimal
+    return dg
+
+
+def score_paths(start_point, target, paths_to_search, polygon, skill, rng,if_print=False):
+    if len(paths_to_search) == 0:
+        return None
+
+    def norm_0_1(arr):
+        if arr.min() == arr.max():
+            return np.ones(arr.shape[0])
+        return (arr - arr.min()) / (arr.max() - arr.min())
+
+    start_to_hole_distance = start_point.distance(target)
+    all_h = np.array([path.heuristic(start_to_hole_distance, skill) for path in paths_to_search])
+    all_c = np.array([path.confidence(polygon, skill, rng) for path in paths_to_search])
+    # all_c = np.where(all_c > 0.8, 0.8, all_c)
+    all_h = norm_0_1(all_h)
+    all_c = norm_0_1(all_c)
+
+    if if_print:
+        l = []
+        for i in range(len(paths_to_search)):
+            l.append((paths_to_search[i].path[0].point.x, paths_to_search[i].path[0].point.y, all_h[i], all_c[i]))
+        l.sort(key = lambda x: x[2] + x[3])
+        print("lps: ", l)
+
+    best_i = np.argmax(all_h + all_c)
+    return paths_to_search[best_i]
+
+
 def direct_distance_angle(curr_loc, target, skill):
     required_dist = curr_loc.distance(target)
     roll_factor = 1.1
     if required_dist < 20:
         roll_factor = 1.0
     max_dist_traveled = 200 + skill
-    distance = min(max_dist_traveled, required_dist / roll_factor)
+    distance = min(max_dist_traveled, (required_dist / roll_factor))
     angle = math.atan2(target.y - curr_loc.y, target.x - curr_loc.x)
 
     return distance, angle
@@ -55,11 +102,13 @@ def direct_distance_angle(curr_loc, target, skill):
 
 def generate_points(curr_loc, target, polygon, skill, increment=25):
     distance, angle = direct_distance_angle(curr_loc, target, skill)
+
     if distance < 20 and line_in_polygon(curr_loc, target, polygon):
         return [LandingPoint(target, distance, angle, curr_loc, target)]
 
-    points = []
-    r = distance
+    target_to_aim = Point(curr_loc.x + distance * np.cos(angle), curr_loc.y + distance * np.sin(angle))
+    points = [LandingPoint(target_to_aim, distance, angle, curr_loc, target)]
+    r = min(distance + 70, 200 + skill)
     while r > 0:
         semicircle_length = np.pi * r
         num_sector = int(semicircle_length / increment)  # divide the semicircle into equally sized sectors
@@ -114,7 +163,7 @@ class LandingPoint(object):
         self.angle_from_origin = angle_from_origin
         self.hole = hole_point
         self.score_threshold = 95
-        self.trials = 20
+        self.trials = 12
         self.start_point = start_point
 
 
@@ -137,16 +186,17 @@ class LandingPoint(object):
             self.shot_confidence = successful / self.trials
             return self.shot_confidence
 
-    def heuristic(self):
-        try:
-            return self.distance_to_hole
-        except AttributeError:
-            self.distance_to_hole = -self.point.distance(self.hole)
-            return self.distance_to_hole
+    def heuristic(self, initial_distance, skill):
+        final_point = get_roll_final_point(self.start_point, self.distance_from_origin, self.angle_from_origin)
+        distance_to_hole = final_point.distance(self.hole)
+        distance_to_hole = 1 if distance_to_hole < 1 else distance_to_hole
+        # self.normalized_hueristic = (initial_distance - distance_to_hole) / (initial_distance)
+        self.h = (200 + skill) / distance_to_hole
+        return self.h
 
-    def score(self, polygon, skill, rng):
+    def score(self, polygon, skill, rng, initial_distance):
         # uses confidence and heuristic
-        return self.heuristic() + (self.confidence(polygon, skill, rng) * 100)
+        return self.heuristic(initial_distance, skill) + (self.confidence(polygon, skill, rng))
 
 
 class MultipleLandingPoints:
@@ -154,8 +204,8 @@ class MultipleLandingPoints:
     def __init__(self, start_lp):
         self.path = [start_lp]
 
-    def heuristic(self):
-        return self.path[-1].heuristic()
+    def heuristic(self, initial_distance, skill):
+        return self.path[-1].heuristic(initial_distance, skill) / len(self.path)
 
     def confidence(self, polygon, skill, rng):
         total = 1
@@ -163,14 +213,15 @@ class MultipleLandingPoints:
             total *= lp.confidence(polygon, skill, rng)
         return total
 
-    def score(self, polygon, skill, rng):
-        return (self.heuristic() + (self.confidence(polygon, skill, rng) * 100))/len(self.path)
+    def score(self, polygon, skill, rng, initial_distance):
+        val = (self.heuristic(initial_distance) + (self.confidence(polygon, skill, rng)))
+        return val/len(self.path)
 
     def add_point(self, polygon, skill, rng):
         last_point = self.path[-1]
 
         landing_points = generate_points(last_point.point, last_point.hole, polygon, skill)
-        next_point= search_landing_points(landing_points, polygon, skill, rng)
+        next_point = score_paths(last_point.point, last_point.hole, landing_points, polygon, skill, rng)
         if next_point:
             self.path.append(next_point)
 
@@ -222,5 +273,16 @@ class Player:
         for path in paths:
             if path.distance_to_hole() > 20:
                 path.add_point(self.shapely_polygon, self.skill, self.rng)
-        largest_point = search_landing_points(paths, self.shapely_polygon, self.skill, self.rng)
+            if path.distance_to_hole()> 20:
+                path.add_point(self.shapely_polygon, self.skill, self.rng)
+        # l = []
+        #
+        # for path in paths:
+        #     l.append((path.path[0].point.x, path.path[0].point.y, path.score(self.shapely_polygon, self.skill, self.rng)))
+        # l.sort(key=lambda x: x[2])
+        # print("Start", curr_loc.x, curr_loc.y)
+        # print("end", target.x, target.y)
+        # print(l)
+        # largest_point = search_landing_points(paths, self.shapely_polygon, self.skill, self.rng)
+        largest_point = score_paths(curr_loc, target, paths, self.shapely_polygon, self.skill, self.rng, if_print=True)
         return largest_point.path[0].distance_from_origin, largest_point.path[0].angle_from_origin
