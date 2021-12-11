8922d68764cbe60eb2046147cf5d174394e02660
Naman Goyal <namangoyal1001@gmail.com>
2021-12-11 13:10:32 -0500

    fixup! Fix precomp dir logic

--- golf_game.py
+++ golf_game.py
@@ -156,7 +156,7 @@ class GolfGame:
                 skill = self.rng.integers(constants.min_skill, constants.max_skill+1)
             self.logger.info("Adding player {} from class {} with skill {}".format(player_name, player_class.__module__, skill))
             precomp_dir = os.path.join("precomp", base_player_name)
-            os.makedirs(precomp_dir, exists_ok=True)
+            os.makedirs(precomp_dir, exist_ok=True)
             player_map_path = slugify(self.golf.map_filepath)
 
             is_timeout = False
